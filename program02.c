b69f17d0aa9d87fefe11c4e1d91199a354300512
seaf <sseaf>
2017-10-22 21:31:46 +0900

    commit 4

---
+++ program02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+  if (n == 0) return 4;
+  if (n == 1) return 1;
+  return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+  printf("Hello world!\n");
+  printf("%d\n", fib(5));
+  printf("%d\n", fib(5));
+  return 0;
+}
+
+<<<<<<< HEAD
+void add(){
+int a;
+}
+=======
+>>>>>>> develop
